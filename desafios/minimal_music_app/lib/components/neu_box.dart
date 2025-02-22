2b47f89e5ad93971d27779828f5fd15457625a5d
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-02-21 23:24:07 -0300

    fix: dark mode

--- desafios/minimal_music_app/lib/components/neu_box.dart
+++ desafios/minimal_music_app/lib/components/neu_box.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_music_app/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 // acho feio demais, mas quero o aprendizado
 class NeuBox extends StatelessWidget {
   final Widget? child;
-
   const NeuBox({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
+    // is dark mode
+    bool isDarkMode = Provider.of<ThemeProvider>(context).isDarkMode;
+
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
@@ -15,14 +19,14 @@ class NeuBox extends StatelessWidget {
         boxShadow: [
           // dark shadow on bottom right
           BoxShadow(
-            color: Colors.grey.shade500,
+            color: isDarkMode ? Colors.black : Colors.grey.shade500,
             blurRadius: 15,
             offset: const Offset(4, 4),
           ),
 
           // light shadown on top left
           BoxShadow(
-            color: Colors.white,
+            color: isDarkMode ? Colors.grey.shade900 : Colors.white,
             blurRadius: 15,
             offset: const Offset(4, 4),
           ),
